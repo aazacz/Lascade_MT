bc8da396b0032e25001ccef4ebd3dc3425d8de5e
aazacz <113631218+aazacz@users.noreply.github.com>
2024-05-10 01:20:35 +0530

    change in env file

--- Config/mongodbServer.js
+++ Config/mongodbServer.js
@@ -18,14 +18,18 @@ const mongoose = require('mongoose')
 //   });
 
 const password = encodeURIComponent(mongoosePassword); //password URLencoding
-const connection = `mongodb+srv://abinbabz01:${password}${mongooseConnectionId}`;
+//connection Id for Atlas
+// const connection = `mongodb+srv://abinbabz01:${password}${mongooseConnectionId}`;
+
+//connection Id for Mongodb Local
+const connection =  'mongodb://127.0.0.1:27017/lascade?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.5';
  
 
  
 
 const connectToMongoDB= mongoose.connect(connection,{})
     .then(()=>{
-    console.log(`Connected to MongoDB Atlas`);
+    console.log(`Connected to MongoDB Local`);
 }).catch((error)=>{
     console.log(error.message);
     console.log("Not Connected");
